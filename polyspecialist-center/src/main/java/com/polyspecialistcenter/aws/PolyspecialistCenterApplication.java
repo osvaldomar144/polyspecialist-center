87da636d39807140bd532c72c101e61bcf242643
Alexis <osvaldomar144@gmail.com>
2022-06-17 11:51:08 +0200

    SetUp project

---
+++ polyspecialist-center/src/main/java/com/polyspecialistcenter/aws/PolyspecialistCenterApplication.java
@@ -0,0 +1,13 @@
+package com.polyspecialistcenter.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyspecialistCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyspecialistCenterApplication.class, args);
+	}
+
+}
---
+++ polyspecialist-center/src/test/java/com/polyspecialistcenter/aws/PolyspecialistCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyspecialistcenter.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyspecialistCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
