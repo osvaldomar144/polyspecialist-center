c3df8388d1b94c9cc921aad61560db23dea38ef7
Alexis <osvaldomar144@gmail.com>
2022-06-19 17:17:13 +0200

    Added profile page

--- polyspecialist-center/src/main/java/com/polyspecialistcenter/aws/controller/AuthenticationController.java
+++ polyspecialist-center/src/main/java/com/polyspecialistcenter/aws/controller/AuthenticationController.java
@@ -1,23 +1,33 @@
 package com.polyspecialistcenter.aws.controller;
 
+import static com.polyspecialistcenter.aws.model.Utente.DIR_FOLDER_IMG;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.polyspecialistcenter.aws.controller.validator.CredentialsValidator;
 import com.polyspecialistcenter.aws.controller.validator.UtenteValidator;
 import com.polyspecialistcenter.aws.model.Credentials;
 import com.polyspecialistcenter.aws.model.Utente;
 import com.polyspecialistcenter.aws.service.CredentialsService;
+import com.polyspecialistcenter.aws.service.UtenteService;
+import com.polyspecialistcenter.aws.utility.FileStore;
 
 @Controller
 public class AuthenticationController {
@@ -26,10 +36,16 @@ public class AuthenticationController {
 	private CredentialsService credentialsService;
 	
 	@Autowired
+	private UtenteService utenteService;
+	
+	@Autowired
 	private CredentialsValidator credentialsValidator;
 	
 	@Autowired
 	private UtenteValidator utenteValidator;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	
 	@RequestMapping(value="/register", method=RequestMethod.GET)
@@ -58,7 +74,7 @@ public class AuthenticationController {
 			return "redirect:/admin/professionisti";
 		}
 		
-		return "authentication/profile";
+		return this.profileUser(model);
 	}
 	
 	@PostMapping(value= {"/register"})
@@ -88,5 +104,61 @@ public class AuthenticationController {
 		OAuth2User userDetails = (OAuth2User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		return "index.html";
 	}*/
+	
+	/* PROFILE */
+	@GetMapping("/profile")
+	public String profileUser(Model model) {
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+		Utente user = utenteService.getUser(credentials.getUtente().getId());
+		model.addAttribute("user", user);
+		model.addAttribute("credentials", credentials);
+		return "authentication/profile";
+	}
+	
+	@PostMapping("/changeUserAndPass/{idCred}")
+	public String changeUserAndPass(@Valid @ModelAttribute("credentials") Credentials credentials,
+									BindingResult credentialsBindingResult,
+									@PathVariable("idCred") Long id,
+									@RequestParam(name = "confirmPass") String pass,								
+									Model model) {
+		
+		credentials.setUsername("defaultUsernameForVa");
+		credentialsValidator.validate(credentials, credentialsBindingResult);
+		
+		if(!credentials.getPassword().equals(pass)) {
+			credentialsBindingResult.addError(new ObjectError("notMatchConfirmPassword", "Le password non coincidono"));
+		}
+		
+		Credentials c = credentialsService.getCredentials(id);
+		Utente user = utenteService.getUser(c.getUtente().getId());
+		credentials.setUsername(c.getUsername());
+		credentials.setId(id);
+		
+		if(!credentialsBindingResult.hasErrors()) {
+			c.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
+			credentialsService.save(c);
+			model.addAttribute("user", user);
+			model.addAttribute("credentials", c);
+			model.addAttribute("okChange", true);
+			return "authentication/profile";
+		}	
+		model.addAttribute("user", user);
+		model.addAttribute("credentials", credentials);
+		model.addAttribute("okChange", false);
+		return "authentication/profile";
+	}
+	
+	@PostMapping("/changeImgProfile/{idUser}")
+	public String changeImgProfile(@PathVariable("idUser") Long id,
+								   @RequestParam("file") MultipartFile file, Model model) {
+		Utente user = utenteService.getUser(id);
+		if(!user.getImg().equals("icon-user-default.png")) {
+			FileStore.removeImg(DIR_FOLDER_IMG, user.getImg());
+		}
+		user.setImg(FileStore.store(file, DIR_FOLDER_IMG));
+		utenteService.save(user);
+		return this.profileUser(model);
+	}
 	
 }
